0e0cf6f78dd1bcb699ef8a8607ec496158827e05
alex <alex@pdp7.net>
2025-07-23 20:18:07 +0200

    Parse OpenSearch XML

--- src/search_portal/__init__.py
+++ src/search_portal/__init__.py
@@ -1,4 +1,5 @@
 import urllib.parse
+from xml.etree import ElementTree
 
 import bs4
 import httpx
@@ -26,3 +27,35 @@ def find_search_on_html(s: str, base_url):
             scheme=href.scheme or base_url.scheme, netloc=href.netloc or base_url.netloc
         )
     )
+
+
+class OpenSearch:
+    """
+    >>> s = OpenSearch(b'<?xml version="1.0"?><OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/"><ShortName>Wikipedia (eu)</ShortName><Description>Wikipedia (eu)</Description><Image height="16" width="16" type="image/x-icon">https://eu.wikipedia.org/static/favicon/wikipedia.ico</Image><Url type="text/html" method="get" template="https://eu.wikipedia.org/w/index.php?title=Berezi:Bilatu&amp;search={searchTerms}" /><Url type="application/x-suggestions+json" method="get" template="https://eu.wikipedia.org/w/api.php?action=opensearch&amp;search={searchTerms}&amp;namespace=0" /><Url type="application/x-suggestions+xml" method="get" template="https://eu.wikipedia.org/w/api.php?action=opensearch&amp;format=xml&amp;search={searchTerms}&amp;namespace=0" /><moz:SearchForm>https://eu.wikipedia.org/wiki/Berezi:Bilatu</moz:SearchForm></OpenSearchDescription>')
+    >>> s.short_name
+    'Wikipedia (eu)'
+    >>> s.urls
+    {'text/html': {'type_': 'text/html', 'method': 'get', 'template': 'https://eu.wikipedia.org/w/index.php?title=Berezi:Bilatu&search={searchTerms}'}, 'application/x-suggestions+json': {'type_': 'application/x-suggestions+json', 'method': 'get', 'template': 'https://eu.wikipedia.org/w/api.php?action=opensearch&search={searchTerms}&namespace=0'}, 'application/x-suggestions+xml': {'type_': 'application/x-suggestions+xml', 'method': 'get', 'template': 'https://eu.wikipedia.org/w/api.php?action=opensearch&format=xml&search={searchTerms}&namespace=0'}}
+    """
+
+    def __init__(self, s):
+        tree = ElementTree.fromstring(s)
+        self.short_name = tree.find(
+            "{http://a9.com/-/spec/opensearch/1.1/}ShortName"
+        ).text
+        self.urls = {
+            u.type_: u
+            for u in map(
+                OpenSearchUrl, tree.findall("{http://a9.com/-/spec/opensearch/1.1/}Url")
+            )
+        }
+
+
+class OpenSearchUrl:
+    def __init__(self, element):
+        self.type_ = element.attrib["type"]
+        self.method = element.attrib["method"]
+        self.template = element.attrib["template"]
+
+    def __repr__(self):
+        return repr(self.__dict__)
